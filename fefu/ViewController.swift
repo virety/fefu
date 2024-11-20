b8949b0b195ffe07a23e1938aff2d515bab34225
virety <152374356+virety@users.noreply.github.com>
2024-11-20 12:56:50 +1000

    Initial Commit

---
+++ fefu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fefu
+//
+//  Created by Вадим Семибратов on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fefuTests/fefuTests.swift
@@ -0,0 +1,17 @@
+//
+//  fefuTests.swift
+//  fefuTests
+//
+//  Created by Вадим Семибратов on 20.11.2024.
+//
+
+import Testing
+@testable import fefu
+
+struct fefuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
